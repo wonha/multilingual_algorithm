876761c236d504157685c992ed0e3879d3bcfb62
wonha.shin <wonhashin26@gmail.com>
2018-01-02 23:58:09 +0900

    Add qsort function type

--- Haskell/qsort.hs
+++ Haskell/qsort.hs
@@ -1,5 +1,6 @@
+qsort :: Ord a => [a] -> [a] -- qsort is a function that maps between list of order-able values 'a'
 qsort [] = []
-qsort (x:xs) = qsort smaller ++ [x] ++ qsort larger
-            where
-                smaller = [a | a <- xs, a <= x]
-                larger = [b | b <- xs, b > x]
+qsort (x:xs) = qsort smaller ++ [x] ++ qsort larger -- operator
+    where -- keywork
+        smaller = [a | a <- xs, a <= x] -- local definition
+        larger = [b | b <- xs, b > x] -- all element from list
--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 This project contains algorithm implementations through various programming languages and models. Functional, Object oriented, and imperative style model will be updated.
 
 # Links
+### VISUALGO
+http://ko.visualgo.net/en
 ### Algorithm Visualizer
 https://github.com/parkjs814/AlgorithmVisualizer
 http://algo-visualizer.jasonpark.me/
-### VISUALGO
-http://ko.visualgo.net/en
 
